c925bfd3eacbf14151d0ce4e6cd5e82a81e7817a
Colin Gourlay <colin.j.gourlay@gmail.com>
2013-12-10 07:55:39 +1000

    More on the footer - sharing icons.

--- src/js/elements/footer.jsx
+++ src/js/elements/footer.jsx
@@ -4,15 +4,25 @@ var utils = require('../utils');
 module.exports = React.createClass({
     render: function () {
         var units = utils.pluralize(this.props.count, 'headline');
-        var count = (
-            <span id="count">
-                {this.props.count}{' '}{units}
-            </span>
+        var shareUrl = encodeURIComponent(window.location);
+        var shareText = encodeURIComponent('There\'s no substitute for good content. That includes headlines.');
+        var tweetHref = 'https://twitter.com/intent/tweet?url=' + shareUrl + '&hashtags=25headlines&related=collypops&text='+ shareText + '&layout=popup';
+        var fbPostHref = 'http://www.facebook.com/sharer.php?s=100&p[url]=' + shareUrl + '&p[summary]=' + shareText + '&layout=popup';
+        var tweet = (
+            <a target="_blank" href={tweetHref} >
+                <span className="ss-icon ss-social-circle ss-twitter"></span>
+            </a>
+        );
+
+        var fbPost = (
+            <a target="_blank" href={fbPostHref} >
+                <span className="ss-icon ss-social-circle ss-facebook"></span>
+            </a>
         );
 
         return (
             <footer id="footer">
-                {count}
+                <span id="share">{tweet}{' '}{fbPost}</span>
                 <span id="credits">
                     <a href="http://twitter.com/collypops">Colin Gourlay</a> 2013
                 </span>
