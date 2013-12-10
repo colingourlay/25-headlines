6960fc66be423e2bf31faa77e3f4bec472ea344c
Colin Gourlay <colin.j.gourlay@gmail.com>
2013-12-10 12:54:26 +1000

    Changed sharing text and added title to FB share.

--- src/js/elements/footer.jsx
+++ src/js/elements/footer.jsx
@@ -5,9 +5,9 @@ module.exports = React.createClass({
     render: function () {
         var units = utils.pluralize(this.props.count, 'headline');
         var shareUrl = encodeURIComponent(window.location);
-        var shareText = encodeURIComponent('There\'s no substitute for good content. That includes headlines.');
+        var shareText = encodeURIComponent('Want a headline worthy of your content? Write 25 and discover your inner genius.');
         var tweetHref = 'https://twitter.com/intent/tweet?url=' + shareUrl + '&hashtags=25headlines&related=collypops&text='+ shareText + '&layout=popup';
-        var fbPostHref = 'http://www.facebook.com/sharer.php?s=100&p[url]=' + shareUrl + '&p[summary]=' + shareText + '&layout=popup';
+        var fbPostHref = 'http://www.facebook.com/sharer.php?s=100&p[url]=' + shareUrl + '&p[title]=25%20Headlines&p[summary]=' + shareText + '&layout=popup';
         var tweet = (
             <a target="_blank" href={tweetHref} >
                 <span className="ss-icon ss-social-circle ss-twitter"></span>
